b9cd389887a385755770bd9843592eee914e7ed5
TheStaplergun <staplergun@gmail.com>
2020-07-14 17:04:53 -0400

    INIT

--- skip_sort_solve.py
+++ skip_sort_solve.py
@@ -4,8 +4,8 @@ import sys
 path = str(sys.argv[1])
 with open(path, "r") as f:
   data = f.readlines()
-  pos = int(data[0]) - 1
+  pos = int(data[0])
   sortable_data = [x for x in data[1:]]
-  sortable_data.sort(key=lambda e:([e[pos] for pos, char in enumerate(e) if pos%4 == 0]))
+  sortable_data.sort(key=lambda e:([e[index] for index, char in enumerate(e) if index%pos == 0]))
   for line in sortable_data:
     print(line[:-1])
